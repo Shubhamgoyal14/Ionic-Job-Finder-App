e182b59eb7ddae468f38bb0883fcb66780a15c38
Shubhamgoyal14 <shubhamgoyal140201@gmail.com>
2024-03-15 17:08:33 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  {
+    apiKey: "AIzaSyA39zClPhkqg1qs_-0O86J3_IytQK21A5Q",
+    authDomain: "ionic-job-finder.firebaseapp.com",
+    projectId: "ionic-job-finder",
+    storageBucket: "ionic-job-finder.appspot.com",
+    messagingSenderId: "725760249742",
+    appId: "1:725760249742:web:98fef4e07097b9e83a1edf",
+    measurementId: "G-4R812VFMHX"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:  {
+    apiKey: "AIzaSyA39zClPhkqg1qs_-0O86J3_IytQK21A5Q",
+    authDomain: "ionic-job-finder.firebaseapp.com",
+    projectId: "ionic-job-finder",
+    storageBucket: "ionic-job-finder.appspot.com",
+    messagingSenderId: "725760249742",
+    appId: "1:725760249742:web:98fef4e07097b9e83a1edf",
+    measurementId: "G-4R812VFMHX"
+  }
 };
 
 /*
